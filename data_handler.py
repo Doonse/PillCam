986d0e8054f2103a8fe6f497f8ac9946c343b861
Doonse <dst036@uit.no>
2024-10-03 21:28:33 +0200

    vgg16 to resnet18

--- data_handler.py
+++ data_handler.py
@@ -11,7 +11,11 @@ te_dataset = datasets.ImageFolder(root='data/DataSet/test', transform=transform)
 va_dataset = datasets.ImageFolder(root='data/DataSet/val', transform=transform)
 
 # Dataloaders
-batch_size = 32
+batch_size = 8
 tr_loader = DataLoader(tr_dataset, batch_size=batch_size, shuffle=True)
 te_loader = DataLoader(te_dataset, batch_size=batch_size, shuffle=True)
 va_loader = DataLoader(va_dataset, batch_size=batch_size, shuffle=True)
+
+
+if __name__ == "__main__":
+    print(tr_dataset.class_to_idx) # {'rest': 0, 'wall': 1}
